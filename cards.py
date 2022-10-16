35b5637980daebb4330d52f6379f37ccab7d9359
David Rochon III <59246317+FutuRePr0Grammer@users.noreply.github.com>
2022-10-16 19:06:25 -0400

    Added classes to get the suit and value of each card

--- cards.py
+++ cards.py
@@ -1,10 +1,38 @@
 import random
 
 
+# class to determine the suit of the card being created
+class Suit:
+    suit = None
+
+    def __init__(self, suit):
+        self.suit = suit
+
+
+# class to determine the value (number) for the card being created
+class CardValue:
+    value = None
+
+    def __init__(self, value):
+        self.value = value
+
+
+# will create a card with a suit and a number
 class Card:
-    pass
+    suit = None
+    value = None
+
+    def __init__(self, suit, value):
+        self.suit = suit
+        self.value = value
+
+    # will make sure doesn't print the address of each card object
+    def __str__(self):
+        return 'Suit: {}, Value = {}'.format(self.suit, self.value)
 
 
+# will create 52 cards and store them in a data structure
+# will also shuffle cards and pull the top card on each call
 class Deck:
     pass
 
